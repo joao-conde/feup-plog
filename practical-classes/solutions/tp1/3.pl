c5192a8880c80a794cc916f6825ee51e3fd24832
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-09-27 13:20:36 +0100

    Exercise 3

--- practical-classes/solutions/tp1/3.pl
+++ practical-classes/solutions/tp1/3.pl
@@ -1,21 +1,22 @@
 livro('Os Maias').
+livro('Harry Potter').
+livro('The Casual Vacancy').
 
-
-autor('Eça de Queiroz','Os Maias').
+autor('Eca de Queiroz','Os Maias').
 autor('J.K. Rowlling','Harry Potter').
 autor('J.K. Rowlling','The Casual Vacancy').
 
-portugues('Eça de Queiroz').
-romance('Os Maias').
-romance('The Casual Vacancy').
-
-escreveu('Eça de Queiroz','Os Maias').
-
-nacionalidade('Eça de Queiroz','portugesa').
+nacionalidade('Eca de Queiroz','portugues').
+nacionalidade('J.K. Rowlling','ingles').
 
 tipo('Os Maias','romance').
 tipo('Harry Potter', 'ficcao').
 tipo('The Casual Vacancy', 'romance').
 
-ingles('J.K. Rowlling').
-ficcao('Harry Potter').
+/*
+    a) autor(X,'Os Maias').
+
+    b) nacionalidade(X,'portugues'), autor(X,_Y), tipo(_Y,'romance').
+
+    c) tipo(_X, 'ficcao'), autor(Y,_X), autor(Y,_Z), tipo(_Z, _T), _T \= 'ficcao'.
+*/
