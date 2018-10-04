9189893102202a7d5b0e4810ff618236a7fe3e92
João Conde <up201503256@fe.up.pt>
2018-10-04 12:36:16 +0100

    1 and 3 tp2

--- practical-classes/solutions/tp2/3.pl
+++ practical-classes/solutions/tp2/3.pl
@@ -1,9 +1,34 @@
 exec(X,Y) :- p(X,Y).
 exec(X,X) :- s(X).
+
 p(X,Y) :- q(X), r(Y).
 p(X,Y) :- s(X), r(Y).
+
 q(a).
 q(b).
 r(c).
 r(d).
 s(e).
+
+
+/*  
+    call 'trace.' on console to debug and watch the program calls
+    call 'notrace.' to turn off debug mode
+
+    exec(X, Y) --> p(X, Y) --> q(X) --> q(X=a) --> r(Y) --> r(Y=c) --> X=a, Y=c ?
+
+    with "no" or ";" (requesting more solutions) --> backtracking
+
+    r(Y) --> r(Y=d) --> X=a, Y=d ; ?
+
+    q(X) --> q(X=b) --> r(Y) --> r(Y=c) --> X=b, Y=c; ?
+                        r(Y) --> r(Y=c) --> X=b, Y=d; ?
+
+    p(X, Y) --> s(X) --> X=e --> r(Y) --> X=e, Y=c; ?
+                                 r(Y) --> X=e, Y=d; ?
+
+    exec(X, X) --> s(X) --> X=e, Y=e; ?
+
+    no (fails)
+
+*/
