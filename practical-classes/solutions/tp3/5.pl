b4abe44f0d97f185feb9e17e527073ae248927b9
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-10-11 12:56:42 +0100

    TP3 - 5

--- practical-classes/solutions/tp3/5.pl
+++ practical-classes/solutions/tp3/5.pl
@@ -1,18 +1,17 @@
-/* a)
+% a) 
 membro(X, [X|_]).
 membro(X, [_|Tail]) :-
-  membro(X, Tail).
-*/
+	membro(X, Tail).
 
+% b) 
+membro2(X,L) :- append(_, [X|_], L).
 
-% b) membro(X,L) :- append(_, [X|_], L).
+% c) 
+lastOne(L,X) :- append(_ ,[X], L).
 
-
-% c) last(L,X) :- append(_ ,[X], L).
-
-/* d)
+% d)
 nth_membro(0, [M|_], M).
 nth_membro(N, [_|T], M) :-
     N>0,
     N1 is N-1,
-*/
+	nth_membro(N1, T, M).
