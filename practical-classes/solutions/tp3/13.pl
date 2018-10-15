9a9ca2368760403905b390e7cc000d5a23acdd26
João Conde <up201503256@fe.up.pt>
2018-10-15 21:12:37 +0100

    tp3 13.pl ints_up_to_n, integers_between, sum_list, is_even, list_evens and list_odds predicates

--- practical-classes/solutions/tp3/13.pl
+++ practical-classes/solutions/tp3/13.pl
@@ -1,13 +1,75 @@
-% a) Construa um predicado lista_ate(N,L) que devolva a lista L de todos os números inteiros
-% entre 1 e N.
-% b) Construa um predicado lista_entre(N1,N2,L) que devolva a lista L de todos os números
-% inteiros entre N1 e N2 (ambos incluídos).
-% Luis Paulo Reis / Eugénio da Costa Oliveira
-% Pág. 19/55c) Construa um predicado soma_lista(L, Soma), que some todos os elementos da lista L,
-% obtendo como resultado Soma.
-% d) Escreva um predicado par(N) que dado um número inteiro N, determine se ele é ou não um
-% número par.
-% e) Escreva um predicado lista_pares(N, Lista) que aceite um número inteiro e que determine a
-% lista de todos os números pares iguais ou inferiores a esse número.
-% f) Escreva um predicado lista_impares(N, Lista) que aceite um número inteiro e que determine
-% a lista de todos os números impares iguais ou inferiores a esse número.
+% a) 
+%TODO: why no work :(
+% integers_up_to(0, []).
+% integers_up_to(N, L):-
+%     N > 0,
+%     N1 is N-1,
+%     integers_up_to(N1, [N|L]).
+
+% a)
+integers_up_to(N, L):-
+    N > 0,
+    build_list(N, 1, L).
+
+build_list(0, _, []).
+build_list(N, X, [X|L]):-
+    N1 is N-1,
+    X1 is X+1,
+    build_list(N1, X1, L).
+
+% b)
+integers_between(N1, N2, L):-
+    build_between_list(N1, N2, L).
+
+build_between_list(N2, N2, [N2]).
+build_between_list(N1, N2, [N1|L]):-
+    N1 =< N2,
+    N3 is N1+1,
+    build_between_list(N3, N2, L).
+
+% c)
+sum_list(L, Sum):-
+    sum_list_aux(L, 0, Sum).
+
+sum_list_aux([], Acc, Acc).
+sum_list_aux([H|T], Acc, Sum):-
+    Acc1 is Acc+H,
+    sum_list_aux(T, Acc1, Sum).
+
+% d)
+is_even(N):-
+    N mod 2 =:= 0.
+
+% e)
+list_evens_up_to(N, L):-
+    list_evens(N, [], L).
+
+list_evens(0, Acc, Acc).
+list_evens(N, Acc, L):-
+    N > 0,
+    is_even(N),
+    N1 is N-1,
+    list_evens(N1, [N|Acc], L).
+
+list_evens(N, Acc, L):-
+    N > 0,
+    \+ is_even(N),
+    N1 is N-1,
+    list_evens(N1, Acc, L).
+
+% f)
+list_odds_up_to(N, L):-
+    list_odds(N, [], L).
+
+list_odds(0, Acc, Acc).
+list_odds(N, Acc, L):-
+    N > 0,
+    N1 is N-1,
+    \+ is_even(N),
+    list_odds(N1, [N|Acc], L).
+
+list_odds(N, Acc, L):-
+    N > 0,
+    N1 is N-1,
+    is_even(N),
+    list_odds(N1, Acc, L).
