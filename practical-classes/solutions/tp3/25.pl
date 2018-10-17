de122831813ba06b511903caeb8390473b5fb4ab
João Conde <up201503256@fe.up.pt>
2018-10-17 18:22:23 +0100

    Pascal triangle, exercise sheet done :D

--- practical-classes/solutions/tp3/25.pl
+++ practical-classes/solutions/tp3/25.pl
@@ -1,10 +1,33 @@
-% Defina o predicado pascal(N, L), onde L é a Nésima linha do triângulo de Pascal:
-%     1
-%    1 1
-%   1 2 1
-%  1 3 3 1
-% 1 4 6 4 1
+%pascal(Level, Line) -->Level of Pascal's triangle to unify with Line
 
-pascal(0, [1]).
+%base cases
 pascal(1, [1]).
-pascal(N, L).
+pascal(2, [1,1]).
+
+pascal(N, L):-
+    Prev is N-1,
+    pascal(Prev, L2),
+    combined_pair_sum(L2, R),
+    append([1|R], [1], L).
+
+
+combined_pair_sum([X,Y], [Z]):- 
+    Z is X + Y.
+
+combined_pair_sum([X,Y|L], Z):- 
+    H is X + Y,
+    Z = [H|L2],
+    combined_pair_sum([Y|L], L2).
+
+
+%display Pascal triangle of N levels
+display_pascal(N):-
+    aux_display(N, 0).
+
+aux_display(N, N).
+aux_display(N, Cnt):-
+    Cnt1 is Cnt+1,
+    Level is Cnt+1,
+    pascal(Level, Row),
+    write(Row), nl,
+    aux_display(N, Cnt1).
