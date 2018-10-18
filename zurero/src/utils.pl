783b9cff64a811d2cde0e5d27835a261c1cf2382
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-10-18 12:58:56 +0100

    Added useful Utils

--- zurero/src/utils.pl
+++ zurero/src/utils.pl
@@ -5,6 +5,24 @@ request_enter:-
 wait_for_enter:-
 	get_char(_).
 
+translate(0,' . ').
+translate(1,' B '). %Black Pieces
+translate(2,' W '). %White Pieces
+
+%predicate to convert letters from rows or columns to an integer
+letter_to_int(Letter, Num) :-
+    Letter @>= 'A',
+    Letter @=< 'Z',
+    char_code('A', AsciiA),
+    char_code(Letter, AsciiL),
+    Num is AsciiL - AsciiA.
+
+letter_to_int(Letter, Num) :-
+    Letter @>= 'a',
+    Letter @=< 'z',
+    char_code('a', AsciiA),
+    char_code(Letter, AsciiL),
+    Num is AsciiL - AsciiA.
 
 %clears console by printing multiple newlines 
 clear_console:- clear_console(20), !.
@@ -13,7 +31,6 @@ clear_console(N):-
 	nl,
 	N1 is N-1,
 	clear_console(N1).
-
 
 %reads the input char and the '\n' character left in input stream
 get_char_nl(Input):-
