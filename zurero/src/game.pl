b7ffad2ecd79334ba74cf1363a405a46d1b58785
João Conde <up201503256@fe.up.pt>
2018-10-23 21:39:27 +0100

    code refactor

--- zurero/src/game.pl
+++ zurero/src/game.pl
@@ -11,8 +11,12 @@ play_game([Board, Player, pvp]):-
     print_board(Board),
     nl, write('It\'s the '), write(Player), write('\'s player turn'), nl,
     get_piece_pos(Column, Row, 'Insert coords'),
-    
-    %pass swapped player turn and new altered board
+    write('Player '), write(Player), write(' choose '), write(Column), write(' - '), write(Row),nl,
+    request_enter,
     switch_turn(Player, NextPlayer),
     play_game([Board, NextPlayer, pvp]).
 
+
+play_game([Board, Player, pvb]).
+
+play_game([Board, Player, bvb]).
