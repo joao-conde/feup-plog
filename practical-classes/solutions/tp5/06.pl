05cc21ed789cb0b8e836353a624514f1308cad0a
João Conde <up201503256@fe.up.pt>
2018-10-25 12:43:50 +0100

    code commenting, removed ai and container pl

---
+++ practical-classes/solutions/tp5/06.pl
@@ -0,0 +1,10 @@
+/*
+
+    The cut in imature acts as a negation, a predicate not. 
+    If X is an adult then imature fails, else it suceeds. 
+    It is a red cut because it is essencial for the program and changes the result.
+
+    The cut in adult avoids exploring all the search space, because X is either a turtle or a person.
+    It is a green cut because it does not change the program outcome but makes it more efficient.
+
+*/
--- zurero/src/board.pl
+++ zurero/src/board.pl
@@ -1,15 +1,19 @@
-print_initial_board :-
-    initial_board(X),  
-    print_board(X).
-
+%%%%%%%%%%%%%%%%%%%%%%
+%   BOARD ELEMENTS   %
+%%%%%%%%%%%%%%%%%%%%%%
 line_numbers(['19','18','17','16','15','14','13','12','11','10',' 9',' 8',' 7',' 6',' 5',' 4',' 3',' 2',' 1']).
 
-print_separator :-
+print_separator:-
     write('  |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n').
 
-print_hline :-
+print_hline:-
     write('  *-------------------------------------------------------------------------------*\n').
 
+/**
+    print_board(-Board)
+
+    Prints the line numbers, game board and separators.
+*/
 print_board(Board) :-
     line_numbers(LineNumbers),
     print_hline,
@@ -18,7 +22,11 @@ print_board(Board) :-
     print_hline,
     write('      A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S\n').
 
-/* Recursive function to print current board state */
+/**
+    print_board_aux(-Board, -LineNumbers)
+
+    Recursive predicate that prints each line with separators
+*/
 print_board_aux([],[]).    
 
 print_board_aux([Line|Board],[LineNumb|Remainder]) :-
@@ -28,7 +36,11 @@ print_board_aux([Line|Board],[LineNumb|Remainder]) :-
     write('--|'), nl,
     print_board_aux(Board,Remainder).
 
-/* Recursive function to print each board's line */
+/**
+    print_line(-Line)
+
+    Recursive predicate that prints each line
+*/
 print_line([]).
 print_line([Head|Tail]) :-
     board_element(Head,T),
@@ -37,19 +49,40 @@ print_line([Head|Tail]) :-
     print_line(Tail).
 
 
+/**
+    player_stone(-Player, +PieceNumber)
+
+    Given a player returns his internal board piece number 
+*/
 player_stone(black, 1).
 player_stone(white, 2).
 
+
+/**
+    direction(-UserInput, +Direction)
+
+    Converts UserInput direction option to a Direction
+*/
 direction(1, top).
 direction(2, right).
 direction(3, bot).
 direction(4, left).
 
+
+/**
+    board_element(-Piece, +Visual)
+
+    Given a Piece number returns a Visual representation to be print in console
+*/
 board_element(0, '-+-').
 board_element(1, '-B-').
 board_element(2, '-W-').
 
-/* Starting game board */
+/**
+    initial_board(-Board)
+
+    Unifies Board with the initial empy game board
+*/
 initial_board([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -74,6 +107,13 @@ initial_board([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 %%%%%%%%%%%%%%%%%%%%%%%
 %   BOARD MODIFIERS   %
 %%%%%%%%%%%%%%%%%%%%%%%
+
+/**
+    set_cell(-Col, -Row, -Elem, -Board, +UpdatedBoard)
+
+    Unifies UpdatedBoard with a Board where at position (Row, Col) there is piece Elem.
+    Makes calls to predicate set_cell_list for each of the rows.
+*/
 set_cell(ElemCol, 0, NewElem, [RowAtTheHead|RemainingRows], [NewRowAtTheHead|RemainingRows]):-
 	set_cell_list(ElemCol, NewElem, RowAtTheHead, NewRowAtTheHead).
 
@@ -82,6 +122,11 @@ set_cell(ElemCol, ElemRow, NewElem, [RowAtTheHead|RemainingRows], [RowAtTheHead|
 	ElemRow1 is ElemRow-1,
 	set_cell(ElemCol, ElemRow1, NewElem, RemainingRows, ResultRemainingRows).
 
+/**
+    set_cell_list(-Idx, -Elem, -Row, +UpdatedRow)
+
+    Unifies UpdatedRow with a Row where at position Idx there is piece Elem
+*/
 set_cell_list(0, Elem, [_|L], [Elem|L]).
 set_cell_list(I, Elem, [H|L], [H|ResL]):-
 	I > 0,
