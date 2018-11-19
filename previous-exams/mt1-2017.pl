d4ffd830a5eb7d8764770ea1de100257d3a1cddd
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-11-19 23:00:39 +0000

    Takes into account a specific case

--- previous-exams/mt1-2017.pl
+++ previous-exams/mt1-2017.pl
@@ -134,7 +134,13 @@ mostEffectivePlayers(Game, Players):-
 					Ratio is Perc / Hours
 					), 
 					PlayersRatios),
-	bestPlayers(PlayersRatios, Players, [], -1).
+    sort(PlayersRatios, SortedPlayersRatios), 
+    /* 
+        sorts to prevent the case of a player having the biggest ratio so far
+        and then another showing up with an even bigger ratio 
+    */
+    reverse(SortedPlayersRatios, NewPlayersRatios),
+	bestPlayers(NewPlayersRatios, Players, [], -1).
 
 bestPlayers([], Players, Players, _).
 bestPlayers([R-P|T], Player, Acc, CurRatio):-
