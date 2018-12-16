7275e76bdfd4bcac1f38e892250bd2072b8d53bb
joao-conde <joaoconde_13@hotmail.com>
2018-12-16 21:39:44 +0000

    some code refactor

--- houses.pl
+++ houses.pl
@@ -2,49 +2,68 @@
 :-use_module(library(clpfd)).
 
 % Solution
-% [0,0] - [0,3]
+% [0,0] - [0,3] 
 % [2,0] - [3,1]
 % [3,0] - [2,1]
 % [2,2] - [3,3]
 puzzle0([[0, 0], [0, 3], [2, 0], [3, 0], [2, 1], [3, 1], [2, 2], [3, 3]]).
 
-%why does solve([[_, _], [_, _], [_, _], [_, _]]). not work DANIEL!=??!
+%why does solve([[_, _], [_, _], [_, _], [_, _]]). not work for puzzle generating?
 
-solve(Houses):-
-    %2 distances
+/*
+    Houses are represented as [x, y] pairs
+    Connections are represented as [[x1, y1], [x2, y2]]
+*/
+connect(Houses):-
+    %There are only 2 distances and they're different
     length(Distances, 2),
     all_distinct(Distances),
+
+    %One connection per 2 houses (pairs) means half the number of houses as connections
     length(Houses, NHouses),
     NConnections #= NHouses // 2,
     length(Connections, NConnections),
-    %house -> x-y
-    %connection -> [x1-y1, x2-y2]
-    %dist(connection) = (x2-x1)^2 + (y2-y1)^2
-    ensure1(Connections, Distances),
-    %append connections = List of houses
-    append(Connections, FlattenedCon),
-    ensure2(Houses, FlattenedCon),
-    ensure3(Connections),
-    append(FlattenedCon, SuperFlattenedCon),
-    labeling([], SuperFlattenedCon),
+    
+    restrictDistances(Connections, Distances), %restrict every connection to have one of the two distances
+    
+    
+    %All the houses must be connected
+    append(Connections, ConnectedHouses),
+    ensureAllConnected(Houses, ConnectedHouses),
+
+    removeSymmetries(Connections), %avoid symmetries
+
+    %flatten list and labeling
+    append(ConnectedHouses, HousesCoordinates),
+    labeling([], HousesCoordinates),
     write(Connections).
 
-ensure1([], _).
-ensure1([[[X1, Y1], [X2, Y2]]|Connections], Distances):-
+/*
+    All distances of all connections are one of the two distances
+    Distance is kept squared to keep it an integer i.e. dist(connection) = dist([[x1, y1], [x2, y2]]) = (x2-x1)^2 + (y2-y1)^2
+*/
+restrictDistances([], _).
+restrictDistances([[[X1, Y1], [X2, Y2]]|Connections], Distances):-
     DiffX #= X2 - X1,
     DiffY #= Y2 - Y1,
     Dis #= DiffX * DiffX + DiffY * DiffY,
     member(Dis, Distances),
-    ensure1(Connections, Distances).
+    restrictDistances(Connections, Distances).
 
-ensure2([], _).
-ensure2([H|Houses], FlattenedCon):-
-    member(H, FlattenedCon),
-    ensure2(Houses, FlattenedCon).
+/*
+    Ensures all houses are connected
+*/
+ensureAllConnected([], _).
+ensureAllConnected([H|Houses], ConnectedHouses):-
+    member(H, ConnectedHouses),
+    ensureAllConnected(Houses, ConnectedHouses).
 
-ensure3([_]).
-ensure3([[[X1, Y1], [X2, Y2]], [[X3, Y3], [X4, Y4]]|Connections]):-
+/*
+    Remove symmetries and connection permutations in final result
+*/
+removeSymmetries([_]).
+removeSymmetries([[[X1, _], [X2, _]], [[X3, Y3], [X4, Y4]]|Connections]):-
     X1 #=< X2,
     X1 #=< X3,
     X3 #=< X4,
-    ensure3([[[X3, Y3], [X4, Y4]]|Connections]).
+    removeSymmetries([[[X3, Y3], [X4, Y4]]|Connections]).
