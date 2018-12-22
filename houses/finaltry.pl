2c277a87898075f930e38d769eb5ac9495088c62
joao-conde <joaoconde_13@hotmail.com>
2018-12-22 17:12:38 +0000

    generator start using restrictions aswell

--- houses/finaltry.pl
+++ houses/finaltry.pl
@@ -1,5 +1,6 @@
 :- use_module(library(lists)).
 :- use_module(library(clpfd)).
+:- use_module(library(random)).
 
 
 puzzle0([[0,0],[0,3],[2,0],[3,0],[2,1],[3,1],[2,2],[3,3]]).
@@ -72,4 +73,52 @@ printSolution(Houses, [I1, I2|Connections]):-
     computeDistance(House1, House2, Distance),
     write('Connection: '), write(House1), write(' <-> '), write(House2), write(' by a distance of '), write(Distance), nl,
     printSolution(Houses, Connections).
+
+
+
+%generator ---------------------------------------------------
+generate(NHouses, _, _):- NHouses mod 2 =\= 0, write('INVALID NUMBER OF HOUSES: must be an EVEN number').
+generate(_, MaxX, _):- MaxX < 0, write('INVALID X DOMAIN RESTRICTION: must be >= 0').
+generate(_, _, MaxY):- MaxY < 0, write('INVALID Y DOMAIN RESTRICTION: must be >= 0').
+
+% generate(NHouses, MaxX, MaxY):-
+%     MaxDis is MaxX * MaxX + MaxY * MaxY,
+%     getRandomDiffNumb(D1, D2, 1, MaxDis),
+
+% getRandomDiffNumb(N1, N2, LB, UB):-
+%     random(LB, UB, N1), %interval [Lower, Upper[
+%     random(LB, UB, N2),
+%     N1 \= N2.
+
+% getRandomDiffNumb(N1, N2, LB, UB):- getRandomDiffNumb(N1, N2, LB, UB).
+    
+
+generate(NHouses, MaxX, MaxY):-    
+    /*
+        let's assume ALL LISTS FLAT 
+            -houses is a list of integers, each 2 integers is (X,Y) of an house
+            -connections refer to houses using indexes of houses list
+            -connections are not a list of lists, but a list of integers, and each 2 consecutive integers  are indexes to house and represent
+            a connection
+    */
+    
+    length(XCoords, NHouses), domain(XCoords, 0, MaxX),
+    length(YCoords, NHouses), domain(YCoords, 0, MaxY),
+
+    ensureDifferentHouseCoords(XCoords, YCoords),
+
+    append(XCoords, YCoords, Coords),
+    labeling([], Coords),
+    write(XCoords), nl, write(YCoords).
+
+
+ensureDifferent(_, [], []).
+ensureDifferent([X1, Y1], [X2|XCoords], [Y2|YCoords]):-
+    X1 #\= X2 #\/ Y1 #\= Y2,
+    ensureDifferent([X1, Y1], XCoords, YCoords).
+
+ensureDifferentHouseCoords([], []).
+ensureDifferentHouseCoords([X|XCoords], [Y|YCoords]):-
+    ensureDifferent([X, Y], XCoords, YCoords),
+    ensureDifferentHouseCoords(XCoords, YCoords).
     
